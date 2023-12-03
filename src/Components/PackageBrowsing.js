a95328c8f10a661abe5023b50780bbccd3b97eb3
yhan4 <yhan.4@hotmail.com>
2023-12-02 20:11:30 -0800

    Initial design for the PackageBrowsing

--- src/Components/PackageBrowsing.js
+++ src/Components/PackageBrowsing.js
@@ -1,7 +1,22 @@
 import React from 'react'
+import '../Styles/PackageBrowsing.css'
 
-export default function PackageBrowsing() {
+function PackageBrowsing({ packages }) {
   return (
-    <div>PackageBrowsing</div>
-  )
+    <div>
+      <h2>Available Travel Packages</h2>
+      {packages.map((packageItem) => (
+        <div key={packageItem.id} className="package-container">
+          <img src={packageItem.pic} alt={packageItem.itinerary.destination} className="package-image" />
+          <h3 className="package-title">{packageItem.itinerary.destination}</h3>
+          <p className="package-details">Duration: {packageItem.itinerary.duration} days</p>
+          <p className="package-details">Price: ${packageItem.price}</p>
+          <p className="package-details">Accommodation: {packageItem.itinerary.accommodation}</p>
+          <p className="package-rating">Overall Rating: {packageItem['overall-rating']}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
+
+export default PackageBrowsing;
