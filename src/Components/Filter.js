5b05858ee37e9b89b3046e85c4f69439464cff1f
yhan4 <yhan.4@hotmail.com>
2023-12-04 12:33:41 -0800

    CSS design for Filter and PackageBrowsing

--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -32,23 +32,23 @@ function Filter({ onFilterChange }) {
         <div className="radio-group">
           <label> 
             <input type="radio" name="priceRange" value="all" onChange={() => onFilterChange('priceRange', { minPrice: '', maxPrice: '' })} />
-            All
+            <span>All</span>
           </label>
           <label>
             <input type="radio" name="priceRange" value="below 1000" onChange={() => onFilterChange('priceRange', { minPrice: '0', maxPrice: '999' })} />
-            Below $1000
+            <span>Below $1000</span>
           </label>
           <label>
             <input type="radio" name="priceRange" value="1000-1999" onChange={() => onFilterChange('priceRange', { minPrice: '1000', maxPrice: '1999' })} />
-            $1000 - $1999
+            <span>$1000 - $1999</span>
           </label>
           <label>
             <input type="radio" name="priceRange" value="2000-3000" onChange={() => onFilterChange('priceRange', { minPrice: '2000', maxPrice: '2999' })} />
-            $2000 - $2999
+            <span>$2000 - $2999</span>
           </label>
           <label>
             <input type="radio" name="priceRange" value="above 3000" onChange={() => onFilterChange('priceRange', { minPrice: '3000', maxPrice: '0' })} />
-            Above $3000
+            <span>Above $3000</span>
           </label>
           
         </div>
