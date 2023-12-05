54f863b6055540de8ceeeace55886172fd5efe63
Esra-Alhussain <esra.alhussain.s@gmail.com>
2023-12-05 11:42:24 -0800

    fetching the data from the mockApi

--- src/Components/PackageDetail.js
+++ src/Components/PackageDetail.js
@@ -1,6 +1,6 @@
 //The PackageDetails component will render the details of a selected travel package. 
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 //({ packageData }) is a destructuring assignment, extracting the packageData property from the props object.
 // equivalent version using the function keyword
@@ -19,9 +19,39 @@ const PackageDetail = ({packageId}) => {
     // State to handle loading state
   const [loading, setLoading] = useState(true);
 
+  //useEffect takes 2 arguments , 1- The function contains the code i want to run as a side effect.
+  useEffect(() => {
+    const fetchData = async ()=> {
+      // Fetch data from the mock API
+      try{
+        const response = await fetch(`https://656ac402dac3630cf7274730.mockapi.io/Travel/packages/{$packageId}`);
+        const data = await response.json();
+
+        //update the state with the fetched data
+        setpackageData(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      } finally{
+        // Set loading to false once data is fetched (or if an error occurs)
+        setLoading(false);
+      }
+    };
+        // Call the fetchData function
+        fetchData();
+        //the second argument of useEffect, is a dependency  array, it consists of packageId variable that useEffect depends on
+        //When packageId in the dependency array changes(if the user selects a different travel package),
+        // the useEffect will be re-run.
+        //the dependency array should not be empty[] because the effect will run once after the initial render
+        // Dependency array ensures useEffect runs when packageId changes
+  }, [packageId]); 
  
   return (
     <div>
+     {loading ? (
+      <p>Loading...</p>
+     ) :(
+      packageData && (
+      <div>
       <p>Duration: {packageData.duration}</p>
       <h2>{packageData.destination}</h2>
       <p>Accomodation: {packageData.accommodation}</p>
@@ -29,7 +59,10 @@ const PackageDetail = ({packageId}) => {
       <p>Tickets Price: {packageData.price}</p>
       <p>Season: {packageData.season}</p>
     </div>
-  )
+      )
+     )}
+     </div>
+  );
 };
 
 export default PackageDetail;
