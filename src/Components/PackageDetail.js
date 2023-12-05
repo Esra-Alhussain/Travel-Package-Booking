67e606039be0fd606e16fbc1bc68e024b75ad025
Esra-Alhussain <esra.alhussain.s@gmail.com>
2023-12-04 22:17:44 -0800

    adding the states to store the package data and to handels the loading data

--- src/Components/PackageDetail.js
+++ src/Components/PackageDetail.js
@@ -1,7 +1,35 @@
-import React from 'react'
+//The PackageDetails component will render the details of a selected travel package. 
 
-export default function PackageBooking() {
+import React, { useState } from 'react';
+
+//({ packageData }) is a destructuring assignment, extracting the packageData property from the props object.
+// equivalent version using the function keyword
+// function PackageDetails(props) {
+//   const { packageData } = props;
+//   // Component logic here
+// }
+//The props object contains all the properties that were passed to the component when it was used.
+//These properties are specified by the parent component that renders the PackageDetails component.
+//packageData as a prop (data for a specific travel package) and manages its internal state using the useState hook
+//<PackageDetails packageData={examplePackage} />
+
+const PackageDetail = ({packageId}) => {
+    // State to store the package data
+  const [packageData, setpackageData] = useState(null);
+    // State to handle loading state
+  const [loading, setLoading] = useState(true);
+
+ 
   return (
-    <div>PackageBooking</div>
+    <div>
+      <p>Duration: {packageData.duration}</p>
+      <h2>{packageData.destination}</h2>
+      <p>Accomodation: {packageData.accommodation}</p>
+      <p>Tickets Available: {packageData.ticketsAvailable}</p>
+      <p>Tickets Price: {packageData.price}</p>
+      <p>Season: {packageData.season}</p>
+    </div>
   )
-}
+};
+
+export default PackageDetail;
