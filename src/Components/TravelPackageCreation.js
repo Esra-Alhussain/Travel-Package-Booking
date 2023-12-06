d8550836912611b3e27a400ed89a2a7467f6ba50
ghofran <gho0ofran.adel14@gmail.com>
2023-12-05 19:54:47 -0800

    description input has been added and design has been finished

--- src/Components/TravelPackageCreation.js
+++ src/Components/TravelPackageCreation.js
@@ -1,54 +1,52 @@
-import React from 'react';
-import { useState } from 'react';
-import '../Styles/TravelPackageCreation.css';
+import React from "react";
+import { useState } from "react";
+import "../Styles/TravelPackageCreation.css";
 
+function TravelPackageCreation({ Packages, setPackages }) {
 
-
-function TravelPackageCreation({Packages,setPackages}) {
-
+  // how  the data should look like when posting 
   const initialPackageState = {
     itinerary: {
-      destination: '',
-      duration: '',
-      season: '',
-      accommodation: '',
+      destination: "",
+      duration: "",
+      season: "",
+      accommodation: "",
     },
-    tickets: '',
-    price: '',
-    pic: '',
+    tickets: "",
+    price: "",
+    pic: "",
+    description:"",
     reviews: [
       {
-        name: '',
-        comment: '',
-        rating: ''
-      }
+        name: "",
+        comment: "",
+        rating: "",
+      },
     ],
-    overallrating: '',
- 
+    overallrating: "",
   };
 
   const [NewPackages, setCreatePackages] = useState(initialPackageState);
-  
-// to handle the change in itinerary ONLY 
+
+  // to handle the change in itinerary ONLY
   const handleInputChange = (event) => {
     const { name, value } = event.target;
-  
+
     setCreatePackages((Packages) => ({
       ...Packages,
       itinerary: {
         ...Packages.itinerary,
         [name]: value,
-      }
-   
+      },
     }));
   };
   // to handle the change in the rest of the object
   const handleInputChangetwo = (event) => {
     const { name, value } = event.target;
-  
     setCreatePackages((Packages) => ({
       ...Packages,
       [name]: value,
+      
     }));
   };
 
@@ -56,13 +54,16 @@ function TravelPackageCreation({Packages,setPackages}) {
     event.preventDefault();
 
     try {
-      const response = await fetch('https://656ac402dac3630cf7274730.mockapi.io/Travel/packages', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(NewPackages),
-      });
+      const response = await fetch(
+        "https://656ac402dac3630cf7274730.mockapi.io/Travel/packages",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(NewPackages),
+        }
+      );
 
       if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
@@ -77,111 +78,122 @@ function TravelPackageCreation({Packages,setPackages}) {
       setCreatePackages(initialPackageState);
 
       // Log the entire response to the console
-      console.log('Server response:', data);
+      console.log("Server response:", data);
     } catch (error) {
       // Handle errors if the POST request fails
-      console.error('Error submitting travel package:', error);
+      console.error("Error submitting travel package:", error);
     }
   };
 
-
-
   return (
-    <div class='container'>
+    <div class="container">
+      <h1>
+        <span>Create Your</span>Traveler’s Experiences
+      </h1>
+      <div className="form">
+        <form onSubmit={handleAddPackage}>
+          {/* Itinerary Section */}
+          <label htmlFor="destination">Destination</label>
+          <input
+            type="text"
+            id="destination"
+            name="destination"
+            placeholder="City, Country"
+            value={NewPackages.itinerary.destination}
+            onChange={handleInputChange}
+            required
+          />
 
-    <h1><span>Create Your</span>Traveler’s Experiences</h1>
-    <div className='form'>
-     <form onSubmit={handleAddPackage}>
-        {/* Itinerary Section */}
-        
-        <label htmlFor="destination">Destination</label>
-        <input
-          type="text"
-          id="destination"
-          name="destination"
-          value={NewPackages.itinerary.destination}
-          onChange={handleInputChange}
-          required
-        />
+          <label htmlFor="duration">Duration</label>
+          <input
+            type="number"
+            min="0"
+            id="duration"
+            name="duration"
+            placeholder="Days"
+            value={NewPackages.itinerary.duration}
+            onChange={handleInputChange}
+            required
+          />
 
-        <label htmlFor="duration">Duration</label>
-        <input
-          type="number"
-          min="0"
-          id="duration"
-          name="duration"
-          value={NewPackages.itinerary.duration}
-          onChange={handleInputChange}
-          required
-        />
+          <label htmlFor="seasons">Season</label>
+          <select
+            id="seasons"
+            name="season"
+            value={NewPackages.itinerary.season}
+            onChange={handleInputChange}
+            required
+          >
+            <option value="Winter">Winter</option>
+            <option value="Spring">Spring</option>
+            <option value="Summer">Summer</option>
+            <option value="Autumn">Autumn</option>
+          </select>
 
-        <label htmlFor="seasons">Season</label>
-        <select
-          id="seasons"
-          name="season"
-          value={NewPackages.itinerary.season}
-          onChange={handleInputChange}
-          required
-        >
-          <option value="Winter">Winter</option>
-          <option value="Spring">Spring</option>
-          <option value="Summer">Summer</option>
-          <option value="Autumn">Autumn</option>
-        </select>
+          <label htmlFor="accommodation">Accommodation</label>
+          <input
+            type="text"
+            id="accommodation"
+            name="accommodation"
+            placeholder="Hotels Or Airbnb"
+            value={NewPackages.itinerary.accommodation}
+            onChange={handleInputChange}
+            required
+          />
 
-        <label htmlFor="accommodation">Accommodation</label>
-        <input
-          type="text"
-          id="accommodation"
-          name="accommodation"
-          value={NewPackages.itinerary.accommodation}
-          onChange={handleInputChange}
-          required
-        />
+          {/* Tickets Section */}
+          <label htmlFor="tickets">Tickets</label>
+          <input
+            type="number"
+            min="0"
+            id="tickets"
+            name="tickets"
+            placeholder="Number of Tickets avalible"
+            value={NewPackages.tickets}
+            onChange={handleInputChangetwo}
+            required
+          />
 
-             
-            
-        {/* Tickets Section */}
-        <label htmlFor="tickets">Tickets</label>
-        <input
-          type="number"
-          min="0"
-          id="tickets"
-          name="tickets"
-          value={NewPackages.tickets}
-          onChange={handleInputChangetwo}
-          required
-        />
+          {/* Price Section */}
+          <label htmlFor="price">Price</label>
+          <input
+            type="number"
+            min="0"
+            id="price"
+            name="price"
+            placeholder="Price only in dollar "
+            value={NewPackages.price}
+            onChange={handleInputChangetwo}
+            required
+          />
 
-        {/* Price Section */}
-        <label htmlFor="price">Price</label>
-        <input
-          type="number"
-          min="0"
-          id="price"
-          name="price"
-          value={NewPackages.price}
-          onChange={handleInputChangetwo}
-          required
-        />
+          {/* Picture Section */}
+          <label htmlFor="pic">Picture</label>
+          <input
+            type="url"
+            id="pic"
+            name="pic"
+            placeholder="Url of the photo"
+            value={NewPackages.pic}
+            onChange={handleInputChangetwo}
+            required
+          />
+          {/* description Section */}
+          <label htmlFor="description">Description</label>
+          <textarea id="description" 
+          name="description" 
+          rows="4" 
+          cols="50"
+          placeholder="Write short description with maximum 150 letter"
+          value={NewPackages.description}
+          onChange={handleInputChangetwo}></textarea>
+          
 
-        {/* Picture Section */}
-        <label htmlFor="pic">Picture</label>
-        <input
-          type="url"
-          id="pic"
-          name="pic"
-          value={NewPackages.pic}
-          onChange={handleInputChangetwo}
-          required
-        />
-        <button type="submit">Add Package</button>
-        
-      </form>
+          <button type="submit">Add Package</button>
+        </form>
       </div>
-      </div>
-    
-  )
+    </div>
+  );
 }
 
-export default TravelPackageCreation
+export default TravelPackageCreation;
