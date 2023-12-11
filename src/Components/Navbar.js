d467238617c0201e0856378488b17712b2d5da6c
ghofran <gho0ofran.adel14@gmail.com>
2023-12-10 21:35:31 -0800

    navbar and herosection are responsive

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,44 +2,35 @@ import React from 'react';
 import "../Styles/Navbar.css";
 import { GrCart } from "react-icons/gr";
 import { Link } from 'react-router-dom';
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
+
   return (
     <div className='Navbar'>
-    <div className='leftside'>
+       <h3 id='navicon' onClick={handleShowNavbar}>
+    <RxHamburgerMenu style={{ fontSize: 'larger' }}  />
+    </h3>
+    <div className={`leftside  ${showNavbar && 'active'}`}>
+      
       <Link to="/">Home</Link>
       <Link to="/AllPackages">Package Browsing</Link>
       <Link to="/TravelPackageCreation">Travel Package Creation</Link>
       <Link to="/About">About</Link>
       <Link to="/Contact">Contact Us</Link>
     </div>
+   
+    
     <h3>
       <GrCart style={{ fontSize: 'larger' }} />
     </h3>
   </div>
-    // <div class='Navbar'>
-    //   <div class='leftside'>
-    //   <Link to="/">Home</Link>
-    //     <Link to="/">
-    //   <h3 to='/PackageBrowsing'>
-    //   Travel package creation
-    //   </h3>
-    //   </Link>
-      
-    //   <h3>
-    //   Packges Browsing
-    //   </h3>
-    //   <h3>
-    //   About 
-    //   </h3>
-    //   <h3>
-    //   Contact Us
-    //   </h3>
-    //   </div>
-    //   <h3>
-    //   <GrCart style={{ fontSize: 'larger' }} />
-    //   </h3>
-      
-    // </div>
+    
   )
 }
