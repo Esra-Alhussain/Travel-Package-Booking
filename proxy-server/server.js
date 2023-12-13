5bdc2426482a2b6c521604beda60d6d71145e1db
Esra-Alhussain <esra.alhussain.s@gmail.com>
2023-12-12 21:38:41 -0800

    Create the Proxy Server

---
+++ proxy-server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+const PORT = 3001; // You can use any port you prefer
+
+// Create a proxy route for the API
+app.use('/api', createProxyMiddleware({
+  target: 'https://656ac402dac3630cf7274730.mockapi.io',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/api': '', // Remove the '/api' prefix when forwarding the request
+  },
+}));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Proxy server is running on http://localhost:${PORT}`);
+});
