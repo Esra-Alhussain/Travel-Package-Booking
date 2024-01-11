3eb0f7e81a5840acb2acaf80ae04d39604754c84
Esra-Alhussain <esra.alhussain.s@gmail.com>
2024-01-11 14:48:05 -0800

    apply the changes for the onePackage component

--- src/pages/OnePackage.js
+++ src/pages/OnePackage.js
@@ -5,62 +5,20 @@ import { useParams } from "react-router-dom"; //to get the package ID from the U
 
 
 function OnePackage({packages }) {
-  //packageData is a state variable that will store the data fetched from the server API.
+  //packageData is a state variable that will store the data fetched from the mockAPI.
   //setPackageData is a function that been provides to update the value of packageData
   //useState(null) initializes packageData with an initial value of null.
-   const [packageData, setPackageData] = useState();
+  //  const [packageData, setPackageData] = useState();
 
    const { id } = useParams();
    const selectedPackage  = packages && packages.find((pkg) => pkg.id === id);
 
-   //fetch the travel package data using useEffect hook or side effects in functional components
-   useEffect(() =>{
-    if (!selectedPackage) {
-      // Handle the case where the selected package is not found
-      console.error(`Package with id ${id} not found`);
-      return;
-    }
-
-      // Keep track of whether the component is mounted
-    let mounted = true;
-
-    const fetchPackageData = async () => {
-      // try block contains the code that attempts to fetch data. If successful, the data is extracted 
-      // using response.json() and then used to update the state variable packageData using setPackageData.
-      try{
-        const response = await fetch(`https://656ac402dac3630cf7274730.mockapi.io/Travel/packages/${selectedPackage.id}`)
-        const data = await response.json(); //extracts the JSON data from the response 
-        // console.log('Fetch URL:', `https://656ac402dac3630cf7274730.mockapi.io/Travel/packages/${selectedPackage.id}`);
-        console.log('Packages:', selectedPackage);
-        console.log('ID:', id);
-
-        // Check if the component is still mounted before updating state
-      if (mounted) {
-        setPackageData(data);//The extracted data is then used to update the packageData state 
-      }
-      }catch (error){
-        console.error('Error fetching the package data', error)
-      }
-    };
-    
-    // Invoke the fetchPackageData function when the component mounts
-    fetchPackageData();
-    
-  return () => {
-    // Cleanup function to set mounted to false when the component unmounts
-    mounted = false;
-  };
-   }, [id,selectedPackage]); // The empty dependency array means this effect runs once when the component mounts
-  //  When this array is empty, the effect runs only once when the component mounts. 
-  //  This is useful for fetching data when the component is initially rendered.
-
-  console.log('Value of packageData:', selectedPackage);
 
    return (
      // compenets and u will pass the data here form this parent to the childerns 
      <div>
       {/* This a ternary conditional expression */}
-      {selectedPackage ? ( // Check if packageData is available before rendering
+      {selectedPackage ? ( // Check if packages is available before rendering
        <>
         <PackageDetail packageItem={selectedPackage}
         />
